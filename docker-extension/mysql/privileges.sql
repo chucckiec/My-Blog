32b9faf36ef5e081693004969bdfcb4c83f1ab1f
cp <chucckiec@gmail.com>
2019-08-11 22:00:12 +0800

    可以用    eclipse for java EE 最新版，tomcat7，jdk1.8，sql数据库、端口设置

--- docker-extension/mysql/privileges.sql
+++ docker-extension/mysql/privileges.sql
@@ -2,6 +2,9 @@ use mysql;
 
 select host, user from user;
 
+drop user tale@'%'; 
+flush privileges; 
+
 create user tale identified by '123456';
 
 grant all on tale.* to tale@'%' identified by '123456' with grant option;
